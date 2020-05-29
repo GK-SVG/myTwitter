2a0c878f1a1ef5d13df54b73478cbddbb39da2e9
gk-svg <gk32239@gmail.com>
2020-05-29 15:03:04 +0530

    my tweet app

---
+++ tweets/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request,'pages/home.html')
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
