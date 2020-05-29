7f6339eed324c79857405353ea6d6755f0bd2747
gk-svg <gk32239@gmail.com>
2020-05-29 15:04:35 +0530

    urls/setting/DB basic changes due to testing template and views

--- myTwitter/urls.py
+++ myTwitter/urls.py
@@ -1,7 +1,6 @@
-"""myTwitter URL Configuration
-
+"""tweetme2 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +13,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path # url()
+
+from tweets.views import home_view, tweet_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('tweets/<int:tweet_id>', tweet_detail_view)
 ]
