fbe68bdcc9b4467de73e7b6e9bfb9d64d2b71032
gk-svg <gk32239@gmail.com>
2020-05-30 07:32:31 +0530

    rest api created for tweets

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,31 @@ from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request,'pages/home.html')
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
