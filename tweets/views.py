870a4a8dabeba5c4985e1fc6f98bfd0f2d9bd2d3
gk-svg <gk32239@gmail.com>
2020-05-31 06:03:01 +0530

    redirect form to home

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import random
 from .models import Tweet
 from .forms import TweetForm
@@ -9,10 +9,14 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    print("next_url", next_url)
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
