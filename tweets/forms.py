40a5f4b2e2293cf6b3daf955083992979cec1d40
gk-svg <gk32239@gmail.com>
2020-05-31 19:48:02 +0530

    migratins and DB

--- tweets/forms.py
+++ tweets/forms.py
@@ -8,8 +8,7 @@ MAX_TWEET_LENGTH = 240
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['content']
-    
+        fields = ['content']  
     def clean_content(self):
         content = self.cleaned_data.get("content")
         if len(content) > MAX_TWEET_LENGTH:
