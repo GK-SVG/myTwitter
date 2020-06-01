a8e9dad35eb880005fbdbc8527e64d2986ea6370
gk-svg <gk32239@gmail.com>
2020-06-01 23:49:54 +0530

    tweet_action_view url added

--- myTwitter/urls.py
+++ myTwitter/urls.py
@@ -20,6 +20,7 @@ from tweets.views import (
     tweet_delete_view,
     tweet_detail_view, 
     tweet_list_view,
+    tweet_action_view,
     tweet_create_view,
 )
 
@@ -29,5 +30,6 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
