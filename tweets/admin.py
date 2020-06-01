042f2262c8bbef3c48319041ef487c8d4c5f3dfc
gk-svg <gk32239@gmail.com>
2020-06-01 22:01:13 +0530

    creatied  like models

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ['id','__str__', 'user']
+    inlines = [TweetLikeAdmin]
+    list_display = ['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
     class Meta:
         model = Tweet
