7e69650b7318cff82d79c56568aebb5b33a22a4c
gk-svg <gk32239@gmail.com>
2020-06-01 06:40:22 +0530

    user model created

--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,22 @@
-from django.db import models
 import random
-# Create your models here.
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
-    content=models.TextField(blank=True,null=True)
-    image=models.ImageField( upload_to='media/images',blank=True,null=True)
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can many tweets
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.content[:25]
     
     class Meta:
         ordering = ['-id']
-
+    
     def serialize(self):
         return {
             "id": self.id,
