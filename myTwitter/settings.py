2b8d35b805534d79f9a2f6e76b1a0fd0f374a3b0
gk-svg <gk32239@gmail.com>
2020-06-01 11:03:04 +0530

    django rest framework added

--- myTwitter/settings.py
+++ myTwitter/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #internal
     'tweets',
+    #third party
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
