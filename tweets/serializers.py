2d57804862692168adb84518dbbf2f6fd7164c55
gk-svg <gk32239@gmail.com>
2020-06-02 06:27:57 +0530

    retweet rest api

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -8,6 +8,7 @@ TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    content = serializers.CharField(allow_blank=True, required=False)
 
     def validate_action(self, value):
         value = value.lower().strip() # "Like " -> "like"
@@ -15,11 +16,14 @@ class TweetActionSerializer(serializers.Serializer):
             raise serializers.ValidationError("This is not a valid action for tweets")
         return value
 
-class TweetSerializer(serializers.ModelSerializer):
+
+class TweetCreateSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = Tweet
         fields = ['id', 'content', 'likes']
+    
     def get_likes(self, obj):
         return obj.likes.count()
     
@@ -27,3 +31,15 @@ class TweetSerializer(serializers.ModelSerializer):
         if len(value) > MAX_TWEET_LENGTH:
             raise serializers.ValidationError("This tweet is too long")
         return value
+
+
+
+class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
+    class Meta:
+        model = Tweet
+        fields = ['id', 'content', 'likes', 'is_retweet', "parent"]
+
+    def get_likes(self, obj):
+        return obj.likes.count()
