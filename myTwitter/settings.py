93c24b2a10c118f609cc42333a6a883b56456743
gk-svg <gk32239@gmail.com>
2020-06-06 22:44:47 +0530

    rendering react by django

--- myTwitter/settings.py
+++ myTwitter/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
 DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
     ]
